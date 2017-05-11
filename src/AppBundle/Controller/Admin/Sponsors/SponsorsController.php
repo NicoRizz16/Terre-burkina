48251c065c93ecebd8daee39d077e0d44c9d0819
NicoRizz16 <nicolas.rizzon@gmail.com>
2017-05-11 20:53:13 +0200

    liste des parrains ajoutée
    Signed-off-by: NicoRizz16 <nicolas.rizzon@gmail.com>

---
+++ src/AppBundle/Controller/Admin/Sponsors/SponsorsController.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Nicolas
+ * Date: 29/04/2017
+ * Time: 10:54
+ */
+
+namespace AppBundle\Controller\Admin\Sponsors;
+
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * @Route("/admin/parrains")
+ * @Security("has_role('ROLE_MODERATOR')")
+ */
+class SponsorsController extends Controller
+{
+    /**
+     * @Route("/{page}", name="admin_sponsors", requirements={"page": "\d+"})
+     */
+    public function indexAction($page = 1)
+    {
+        if($page<1){
+            throw new NotFoundHttpException('Page "'.$page.'"inexistante.');
+        }
+
+        $repository = $this->getDoctrine()->getManager()->getRepository('AppBundle:User');
+
+        // Récupération de la liste des utilisateurs pour la page demandée
+        $nbPerPage = User::NUM_ITEMS;
+        $sponsorsList = $repository->getSponsors($page, $nbPerPage);
+        $nbPageTotal = ceil(count($sponsorsList)/$nbPerPage);
+
+        if($page>$nbPageTotal && $page != 1){
+            throw $this->createNotFoundException('La page "'.$page.'" n\'existe pas.');
+        }
+
+        return $this->render('admin/sponsors/sponsors/index.html.twig', array(
+            'sponsorsList' => $sponsorsList,
+            'nbPageTotal' => $nbPageTotal,
+            'page' => $page
+        ));
+    }
+
+}
--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -29,4 +29,24 @@ class UserRepository extends \Doctrine\ORM\EntityRepository
         return new Paginator($query, true);
     }
 
+    public function getSponsors($page, $nbPerPage)
+    {
+        $query = $this->createQueryBuilder('u')
+            ->andWhere('u.roles LIKE :roles')
+            ->setParameter('roles', '%ROLE_SPONSOR%')
+            ->orderBy('u.username', 'ASC')
+            ->getQuery()
+            ;
+
+        $query
+            // On définit l'annonce à partir de laquelle commencer la liste
+            ->setFirstResult(($page-1)*$nbPerPage)
+            // Ainsi que le nombre d'annonce à afficher sur une page
+            ->setMaxResults($nbPerPage)
+        ;
+
+        // On retourne l'objet Paginator correspondant à la requête construite
+        return new Paginator($query, true);
+    }
+
 }
