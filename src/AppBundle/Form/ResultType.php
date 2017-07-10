984a00b817ff597276e9064f4fb11f49abf7b986
NicoRizz16 <nicolas.rizzon@gmail.com>
2017-07-10 14:47:35 +0200

    Correction bug ajout résultat filleul
    Signed-off-by: NicoRizz16 <nicolas.rizzon@gmail.com>

--- src/AppBundle/Form/ResultType.php
+++ src/AppBundle/Form/ResultType.php
@@ -2,9 +2,9 @@
 
 namespace AppBundle\Form;
 
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
